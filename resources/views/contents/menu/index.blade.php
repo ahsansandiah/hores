bce42919a552413f2b87d0ff407bd292263a3a8f
unknown <radhian21@gmail.com>
2019-06-16 18:57:23 +0700

    update view menu

--- resources/views/contents/menu/index.blade.php
+++ resources/views/contents/menu/index.blade.php
@@ -8,34 +8,55 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-            	<div class="row">
-                    <div class="col-md-2">
-                        <button class="btn btn-sm btn-primary" onclick="addMenu()">
-                            Add Menu
-                        </button>
-                    </div>
-                    <div class="col-md-10">
+            	<button class="btn btn-sm btn-primary" onclick="addMenu()">
+                    Add Menu
+                </button>
+            	<div class="row" style="margin-top: 30px;">
+                    <div class="col-md-12">
                         <div id="form-create" class="collapse">
                             <form method="post" id="menu-form" action="{{ url('admin/menu') }}">
                                 @csrf
                                 <div class="row">
-                                    <div class="col-md-5">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <select class="form-control" name="parent_id" id="parent-id">
+                                            	<option value="">- No Parent -</option>
+                                            	@if ($menus)
+                                            		@foreach ($menus as $menu)
+                                            			@if (empty($menu->parent_id))
+                                            				<option value="{{ $menu->id }}">
+                                            					{{ $menu->name }}
+                                            				</option>
+                                            			@endif
+                                            		@endforeach
+                                            	@endif
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Name</label>
                                             <input type="text" name="name" id="menu-name" class="form-control" placeholder="Menu Name" required>
                                         </div>
                                     </div>
-                                    <div class="col-md-5">
+                                    <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Route</label>
-                                            <input type="text" name="route" id="menu-route" class="form-control" placeholder="Menu Description" required>
+                                            <input type="text" name="route" id="menu-route" class="form-control" placeholder="Menu Description">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Icon</label>
+                                            <input type="text" name="icon" id="menu-icon" class="form-control" placeholder="Menu Icon">
                                         </div>
                                     </div>
                                     <div class="col-md-2">
-                                        <button class="btn btn-sm btn-primary" style="margin-top:30px;">
-                                            Submit
-                                        </button>
-                                    </div>
+		                                <button class="btn btn-sm btn-primary">
+		                                    Submit
+		                                </button>
+		                            </div>
                                 </div>
                             </form>
                         </div>
@@ -45,18 +66,25 @@
                 <table class="table table-bordered">
                 	<thead>
                 		<tr>
+                			<th>Parent</th>
                 			<th>Name</th>
                 			<th>Slug</th>
                 			<th>Route</th>
+                			<th>Action</th>
                 		</tr>
                 	</thead>
                 	<tbody>
                 		@if ($menus)
                 			@foreach ($menus as $menu)
                 				<tr>
+                					<td>{{ $menu->parent_name }}</td>
                 					<td>{{ $menu->name }}</td>
                 					<td>{{ $menu->slug }}</td>
                 					<td>{{ $menu->route }}</td>
+                					<td>
+                						<button class="btn btn-sm btn-warning" onclick="editMenu(<?=$menu->id?>, <?=empty($menu->parent_id) ? 0 : $menu->parent_id?>, '<?=$menu->name?>', '<?=$menu->route?>', '<?=$menu->icon?>')">Edit</button>
+                						<button class="btn btn-sm btn-danger">Delete</button>
+                					</td>
                 				</tr>
                 			@endforeach
                 		@endif
@@ -74,6 +102,8 @@
 		function addMenu(){
 			$('#menu-name').val('');
 			$('#menu-route').val('');
+			$('#parent-id').val('');
+			$('#menu-icon').val('');
 			var url = window.location.origin + '/admin/menu';
 			$('#menu-form').attr('action', url);
 
@@ -82,6 +112,16 @@
 			} else {
 				$('#form-create').collapse('show');
 			}
+		}
+
+		function editMenu(id, parent_id, name, route, icon){
+			$('#menu-name').val(name);
+			$('#menu-route').val(route);
+			$('#parent-id').val(parent_id);
+			$('#menu-icon').val(icon);
+			var url = window.location.origin + '/admin/menu/update/' + id;
+			$('#menu-form').attr('action', url);
+			$('#form-create').collapse('show');
 		}
 	</script>
 @endsection
