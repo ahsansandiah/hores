e77591ecdf850302e715bdd107c5a3b79c8bc6da
unknown <radhian21@gmail.com>
2019-06-16 17:51:37 +0700

    add action model

---
+++ app/Action.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Action extends Model
+{
+    protected $table = 'actions';
+
+    protected $fillable = [
+    	'name',
+    	'slug'
+    ];
+}
