c1ab38174dfb3b9228a260bfb5d96e59585f97ef
unknown <radhian21@gmail.com>
2019-06-16 18:57:07 +0700

    update menu model

--- app/Menu.php
+++ app/Menu.php
@@ -9,8 +9,34 @@ class Menu extends Model
     protected $table = 'menus';
 
     protected $fillable = [
+    	'parent_id',
     	'name',
     	'slug',
-    	'route'
+    	'route',
+    	'icon'
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'parent_name'
+    ];
+
+    // get parent name attribute
+    public function getParentNameAttribute(){
+    	$parent_id = $this->attributes['parent_id'];
+    	if (!empty($parent_id)) {
+    		$parent = Menu::where('id', $parent_id)->first();
+    		if ($parent) {
+    			return $parent->name;
+    		}
+
+    		return '';
+    	}
+
+    	return '';
+    }
 }
