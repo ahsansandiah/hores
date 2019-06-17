94a1a6b3cf2d5151e32283a3a27758840615bdbe
unknown <radhian21@gmail.com>
2019-06-17 10:53:25 +0700

    add composer service provider

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Using class based composers...
+        View::composer('partials.left-side-menu-admin', 'App\Http\ViewComposers\AdminMenuComposer');
+    }
+}
