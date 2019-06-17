0dbe3672806860139c3ee39f5b59b2947616f6bd
unknown <radhian21@gmail.com>
2019-06-17 10:54:28 +0700

    update sidebar menu become dynamic

--- resources/views/partials/left-side-menu-admin.blade.php
+++ resources/views/partials/left-side-menu-admin.blade.php
@@ -1,6 +1,36 @@
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header"></li>
-    <li style="treeview">
+
+    @if (isset($menu_sidebar) && $menu_sidebar)
+        @foreach ($menu_sidebar as $ms)
+            @if (count($ms->children) > 0)
+                <li class="treeview">
+                    <a href="#">
+                        <i class="{{ $ms->icon }}"></i>
+                        <span>{{ $ms->name }}</span>
+                        <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu" style="">
+                        @foreach ($ms->children as $child)
+                            <li><a href="{{ url($child->route) }}">
+                                <i class="{{ $child->icon }}"></i> {{ $child->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            @else
+                <li>
+                    <a href="{{ url(empty($ms->route) ? '/' : $ms->route) }}">
+                        <i class="{{ $ms->icon }}"></i>
+                        <span>{{ $ms->name }}</span>
+                    </a>
+                </li>
+            @endif
+        @endforeach
+    @endif
+    <!-- <li style="treeview">
         <a href="{{ Url::to('/') }}">
             <i class="fa fa-puzzle-piece"></i>
             <span>Dashboard</span>
@@ -32,26 +62,28 @@
             <i class="fa fa-cog"></i>
             <span>Additional Services</span>
         </a>
-    </li>
-    <li class="treeview">
-        <a href="#">
-            <i class="fa fa-cog"></i>
-            <span>Config</span>
-            <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu" style="">
-            <li><a href="{{ url('admin/roles') }}"><i class="fa fa-users"></i> Roles</a></li>
-            <li><a href="{{ url('admin/menus') }}"><i class="fa fa-list"></i> Menus</a></li>
-            <li><a href="{{ url('admin/actions') }}"><i class="fa fa-key"></i> Actions</a></li>
-        </ul>
-    </li>
-    <li style="treeview">
-        <a href="{{ Url::to('admin/users') }}">
-            <i class="fa fa-users"></i>
-            <span>Users</span>
-        </a>
-    </li>
+    </li> -->
+    @if (Auth::user()->hasRole(['admin']))
+        <li class="treeview">
+            <a href="#">
+                <i class="fa fa-cog"></i>
+                <span>Config</span>
+                <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+                </span>
+            </a>
+            <ul class="treeview-menu" style="">
+                <li><a href="{{ url('admin/roles') }}"><i class="fa fa-users"></i> Roles</a></li>
+                <li><a href="{{ url('admin/menus') }}"><i class="fa fa-list"></i> Menus</a></li>
+                <li><a href="{{ url('admin/actions') }}"><i class="fa fa-key"></i> Actions</a></li>
+            </ul>
+        </li>
+        <li style="treeview">
+            <a href="{{ Url::to('admin/users') }}">
+                <i class="fa fa-users"></i>
+                <span>Users</span>
+            </a>
+        </li>
+    @endif
 </ul>
     
